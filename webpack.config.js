07518105f86f126143556235fe7db8be3b5c85b3
KennethVask <Kenneth.Vask@tptlive.ee>
2025-10-09 15:04:00 +0300

    Reset project

--- webpack.config.js
+++ webpack.config.js
@@ -2,27 +2,13 @@ import path from 'path';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import { stripVTControlCharacters } from 'util';
 
-let response = await fetch ('https://rickandmortyapi.com/api/character/');
-let data = await response.json();
-console.log(data);
-
-let pages = [];
-for(let character of data.results){
-  let page =   new HtmlWebpackPlugin({
-      filename:`${character.id}.html`,
-      template: './src/views/page.njk',
-      templateParameters: {
-        character, // same as character: character
-      }
-    });
-    pages.push(page);
-}
 
 export default {
   entry: './src/index.js',
   output: {
     filename: 'main.js',
     path: path.resolve(import.meta.dirname, 'dist'),
+    clean: true,
   },
   devServer: {
     static: {
@@ -52,31 +38,11 @@ export default {
           }
         ],
       },
-      {
-        test: /\.njk$/,
-        use: [
-          {
-            loader: 'simple-nunjucks-loader',
-            options: {}
-          }
-        ]
-      }
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/views/index.njk',
-      templateParameters: {
-        characters: data.results
-      }
+      template: './src/index.html',
     }),
-    new HtmlWebpackPlugin({
-      filename: 'About.html',
-      template: './src/views/About.njk',
-    }), new HtmlWebpackPlugin({
-      filename: 'Contacts.html',
-      template: './src/views/Contacts.njk',
-    }),
-    ...pages
   ],
 };
