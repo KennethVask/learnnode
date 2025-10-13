bbb8a09ca130a5559b06bb5af982398234740ac7
Kenneth Vask <Kenneth.Vask@tptlive.ee>
2025-10-13 15:54:12 +0300

    Add ItemList component

---
+++ src/ItemList.vue
@@ -0,0 +1,12 @@
+<script setup>
+defineProps(['items' , 'titel']);
+</script>
+<template>
+    <h1>{{ titel }}</h1>
+    <ul>
+        <li v-for="item in items" :key="item.id">
+            <input type="checkbox" v-model="item.isDone">
+            {{ item.name }}
+        </li>
+    </ul>
+</template>
