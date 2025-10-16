72f1599c2b44349fd1a0ac85420b9403960af4ba
KennethVask <Kenneth.Vask@tptlive.ee>
2025-10-16 13:07:52 +0300

    Helping Jaskar

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,9 @@
 <script setup>
 import { ref } from 'vue';
+import Modal from './components/Modal.vue';
     let modalActive = ref(false)
 </script>
 <template>
     <button class="button is-primary" @click="modalActive=true">Open Modal</button>
-
-    <div class="modal" :class="{'is-active': mo}" >
-        <div class="modal-background" @click="modalActive=flase"></div>
-        <div class="modal-content">
-            <p class="image is-4by3">
-                <img src="https://bulma.io/assets/images/placeholders/1280x960.png" alt="">
-            </p>
-        </div>
-        <button class="modal-close is-large" aria-label="close" @click="modalActive=flase"></button>
-    </div>
+    <Modal></Modal>
 </template>
