99e27f7b0c13420b58d5a80e0498837f4c3bd092
Kenneth Vask <Kenneth.Vask@tptlive.ee>
2025-10-20 16:00:35 +0300

    commit unfinished

--- src/pages/ChuckNorris.vue
+++ src/pages/ChuckNorris.vue
@@ -1,7 +1,10 @@
 <script setup>
-
+let res = await fetch('https://api.chucknorris.io/jokes/random');
+let data = await res.json();
+console.log(data);
+console.log('Hello');
 </script>
 
 <template>
-    
+
 </template>
