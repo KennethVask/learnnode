d4e09b6c7eb7c84f7c05491cb71e77f9f3705f4a
Kenneth Vask <Kenneth.Vask@tptlive.ee>
2025-11-20 16:00:52 +0200

    Add boxen

--- bin/terminal.js
+++ bin/terminal.js
@@ -7,21 +7,5 @@ import chalk from 'chalk';
 write(chalk.red.bgYellow.strikethrough('DANGER!!!!!'));
 write(chalk.bgAnsi256(67)('COLOR'));
 write(chalk.bgRgb(255,0,0)('COLOR'));
+write('\n')
 
-import inquirer from 'inquirer';
-
-let anwsers = await inquirer.prompt([
-    {
-        type: 'input',
-        name: 'name',
-        message: 'What is your name?',
-    },
-        {
-        type: 'number',
-        name: 'age',
-        message: 'Waht is your age?',
-    },
-]);
-
-console.log(`Hello ${anwsers.name}!!!!`);
-console.log(`You are ${anwsers.age} years old!`);
